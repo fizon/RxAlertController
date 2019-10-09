5b399d9f9ab06eef4c31cca01751bf043d2cf048
Jeremy Boy <github@jboy.eu>
2019-10-09 17:26:23 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "RxAlertController",
+	platforms: [
+		.iOS(.v9)
+	],
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
+			name: "RxAlertController",
+			targets: ["RxAlertController"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
+	],
+	targets: [
+		.target(
+			name: "RxAlertController",
+			dependencies: ["RxSwift"],
+			path: "RxAlertController"),
+	]
+)
